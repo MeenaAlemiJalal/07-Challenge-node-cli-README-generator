7a270fa21e9a3cdb46f02c9c073a1b1abb21cc5e
meenaalemijalal <meena.maj58@gmail.com>
2021-12-04 19:11:04 -0800

    readme file structure updated

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,3 +1,7 @@
+const MarkdownIt = require("markdown-it");
+
+const md = new MarkdownIt()
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
@@ -13,7 +17,7 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return`
-    # ${data.title}
+    ${md.render('# ${data.title}')}
 
     ## Description
     ### ${data.description}
